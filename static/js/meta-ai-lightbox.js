5247075838115e04fdcf377f4d27d4629d4111d5
Bersen Naidoo <bersen.naidoo@gmail.com>
2025-03-07 11:43:55 +0200

    Added temple photos to gallery

--- static/js/meta-ai-lightbox.js
+++ static/js/meta-ai-lightbox.js
@@ -1,10 +1,10 @@
-const lightbox = new Lightbox({
-    albumLabel: 'Image %1 of %2',
-    alwaysShowNavOnTouchDevices: true,
-    fadeDuration: 600,
-    imageFadeDuration: 600,
-    positionFromTop: 50,
-    resizeDuration: 700,
-    showImageNumberLabel: true,
-    wrapAround: true
-  });
+lightbox.option({
+  albumLabel: "Image %1 of %2",
+  alwaysShowNavOnTouchDevices: true,
+  fadeDuration: 600,
+  imageFadeDuration: 600,
+  positionFromTop: 50,
+  resizeDuration: 700,
+  showImageNumberLabel: true,
+  wrapAround: true,
+});
